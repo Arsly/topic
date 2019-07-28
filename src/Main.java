3b0b845dbb7aa13d35ade4ebf78fc69bbb15fcde
Arsly <10525123@nhu.edu.tw>
2019-07-28 11:36:18 +0800

    啟動檔案上限功能，PDF不受檔案大小限制

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@
 import org.apache.pdfbox.pdmodel.*;
 //_02_匯入pdf文字解析工具
 import org.apache.pdfbox.text.PDFTextStripper;
-
+//_03_記憶體調適
+import org.apache.pdfbox.io.MemoryUsageSetting;
+//_04_呼叫檔案工具
 import java.io.File;
 
 public class Main {
@@ -10,8 +12,8 @@ public class Main {
         //指定檔案位置，解析文件放在專案根目錄
         File pdffile = new File("./test.pdf");
         try {
-            //使用PDDocument設定document，載入pdf文件內容
-            PDDocument document = PDDocument.load(pdffile);
+            //使用PDDocument設定document，載入pdf文件內容，setupMainMemoryOnly啟動檔案上限功能，不受檔案大小限制
+            PDDocument document = PDDocument.load(pdffile,MemoryUsageSetting.setupMainMemoryOnly());
             //使用PDFTextStripper設定Stripper為文字解析工具
             PDFTextStripper Stripper = new PDFTextStripper();
             //解析進行時，依照內容由上到下排序
